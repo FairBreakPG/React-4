1747f690433bbcb138d83534b06cef8332b21c80
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-05-08 00:57:45 -0600

    traer datos desde el api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [informacionApi, setearInformacionApi] = useState([])
+
+  //funcion de consultar Api
+  useEffect(() =>{
+      consultarApi();
+  }, []);
+
+  const consultarApi = async () => {
+    const url = "https://genshin.jmp.blue/characters/all?lang=es";
+    const respuesta = await fetch(url);
+    const datos  = await respuesta.json();
+    console.log("monas chinas", datos);
+  }
 
   return (
     <>
